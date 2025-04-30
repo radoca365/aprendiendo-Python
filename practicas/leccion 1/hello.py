0c3b4c89cd8861ab60d956abc7379a2dee92b148
radoca365 <radoca365@gmail.com>
2025-04-30 01:48:18 -0700

    Initial commit

---
+++ practicas/leccion 1/hello.py
@@ -0,0 +1,12 @@
+# este programa es mi primer  programa en python aqui colocare un saludo al mundo :D
+print("HOLA MUNDO")
+"""
+este es un comentario
+en varias
+lineas
+"""
+# consultar tipo de dato en python
+print(type("soy un dato str")) # este  comando con sulta el tipo de dato  arrojara tipo 'str'
+print(type (5)) # este dato es un dato entero int
+print(type (1.5)) # este dato es un dato decimal float
+print(type (False)) # este dato es un dato booleano bool
